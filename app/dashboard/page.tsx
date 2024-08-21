3b3084cc11baf9e85a1149972acd0a7296f13fc4
pyoja <wjdgns13nr@naver.com>
2024-08-21 13:58:07 +0900

    login singup update

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import TodoList from "../../components/TodoList";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <TodoList />
+    </div>
+  );
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import SignupForm from "../../components/SignupForm";
+
+export default function Signup() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <SignupForm />
+    </div>
+  );
+}

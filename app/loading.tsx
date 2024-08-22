4dfbca0db4ce9a0d2407103daf64119eb7138ce1
pyoja <wjdgns13nr@naver.com>
2024-08-22 10:34:27 +0900

    loading & not-found

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div>
+      <h1>Loading...</h1>
+      <p>페이지를 불러오는 중입니다. 잠시만 기다려 주세요.</p>
+    </div>
+  );
+};
+
+export default Loading;
---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>요청하신 페이지는 존재하지 않습니다.</p>
+    </div>
+  );
+};
+
+export default NotFound;
